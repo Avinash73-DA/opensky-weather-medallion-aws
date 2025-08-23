728202860f0a6373a95674af226e547ac396ff6d
Avinash_DA <avinashsolai@gmail.com>
2025-08-23 15:22:32 +0530

    Update bronze_layer_ingestion_dag.py
    Silver Layer Dag for Glue Jobs has been added and time interval is 10 Minutes

--- airflow_dags/bronze_layer_ingestion_dag.py
+++ airflow_dags/bronze_layer_ingestion_dag.py
@@ -1,6 +1,7 @@
 from airflow.models.dag import DAG
 from datetime import datetime,timedelta
 from airflow.providers.amazon.aws.operators.lambda_function import LambdaInvokeFunctionOperator
+from airflow.providers.amazon.aws.operators.glue import GlueJobOperator
 
 default_args = {
     'owner':'Avinash',
@@ -14,7 +15,7 @@ with DAG(
     dag_id = "lambda_bronze_ingestion_dag",
     default_args=default_args,
     description="DAG to invoke Lambda function for bronze ingestion",
-    schedule_interval="@daily",
+    schedule_interval=timedelta(minutes=10),
     start_date=datetime(2025,8,17),
     catchup=False,
     tags=["aws","lambda","bronze","ingestion"]
@@ -29,5 +30,24 @@ with DAG(
         task_id="invoke_lambda_weather",
         function_name="weatherDataPull",
     )
+
+    ## Transformation for Silver Layer
+    process_plane_data = GlueJobOperator(
+        task_id="process_plane_data",
+        job_name="planes_silver",
+        wait_for_completion=True
+    )
     
-    lambda_flights >> lambda_weather
+    process_weather_data = GlueJobOperator(
+        task_id="process_weather_data",
+        job_name="openweather_silver",
+        wait_for_completion=True
+    )
+    
+    process_enriched_data = GlueJobOperator(
+        task_id="process_enriched_data",
+        job_name="open_sky_weather_enriched_silver",
+        wait_for_completion=True
+    )
+    
+    [lambda_flights,lambda_weather] >> [process_plane_data,process_weather_data,process_enriched_data]
